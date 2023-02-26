13a72093bf6b5805fc802408d42cc9ca4e338a29
기강조 <120554102+chogigang@users.noreply.github.com>
2023-02-27 00:01:09 +0900

    Add files via upload
    학교 입학 3일전 자바 전체 복습

--- src/org/opentutorials/javatutorials/Stream/_01.java
+++ src/org/opentutorials/javatutorials/Stream/_01.java
@@ -1,5 +1,7 @@
 package Stream;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class _01 {
@@ -10,5 +12,39 @@ public class _01 {
     Stream<String> sortedStream = stream.sort(); // 정렬 (중간 연산)
     Stream<String> limitedStream = stream.limit(5); // 스트림 자르기 (중간 연산)
    // int total = stream.count(); // 요소 개수 세기 (최종 연산) - 스트림 닫힘 closed
+
+
+
+
+    Stream<T> Collection.stream()
+
+    // List로부터 스트림 생성
+    List<Integer> list = Arrays.asList(1,2,3,4,5);
+    Stream<Integer> intStream = list.stream();  // list를 소스로 하는 컬렉션 생성
+
+
+
+    //스트림 배열
+
+    Stream<T> Stream.of(T... values)  // 가변 인자
+    Stream<T> Stream.of(T[])
+    Stream<T> Arrays.stream(T[])
+    Stream<T> Arrays.stream(T[] array, int startInclusive, int endExclusive)
+    // 문자열 스트림 생성
+    Stream<String> strStream = Stream.of("a","b","c"); // 가변인자
+    Stream<String> strStream = Stream.of(new String[]{"a","b","c"});
+    Stream<String> strStream = Arrays.stream(new String[]{"a","b","c"});
+    Stream<String> strStream = Arrays.stream(new String[]{"a","b","c"}, 0, 3);
+    // int, long, double과 같은 기본형 배열을 소스로 하는 스트림 생성
+    IntStream IntStream.of(int...values)
+    IntStream IntStream.of(int[])
+    IntStream Arrays.stream(int[])
+    IntStream Arrays.stream(int[] array, int startInclusive, endExclusive)
+
+        intStream Array.stream(int[] array, int startInclusive, endExclusive)
+
+
+
+
 
 }
